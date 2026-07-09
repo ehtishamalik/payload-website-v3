7485e2205c547ec8845027ee3b21596520f0d5d9
ehtishamalik <malikehtishamali334@hotmail.com>
2026-07-09 16:29:48 +0500

    formatted

--- src/Header/Component.tsx
+++ src/Header/Component.tsx
@@ -1,9 +1,8 @@
-import { HeaderClient } from './Component.client'
-import { getCachedGlobal } from '@/utilities/getGlobals'
-import React from 'react'
+import { getCachedGlobal } from "@/utilities/getGlobals";
+import { HeaderClient } from "./Component.client";
 
 export async function Header() {
-  const headerData = await getCachedGlobal('header', 1)()
+	const headerData = await getCachedGlobal("header", 1)();
 
-  return <HeaderClient data={headerData} />
+	return <HeaderClient data={headerData} />;
 }
--- src/blocks/Banner/Component.tsx
+++ src/blocks/Banner/Component.tsx
@@ -1,27 +1,27 @@
-import React from 'react'
-import RichText from '@/components/RichText'
+import type React from "react";
+import RichText from "@/components/RichText";
 
-import { cn } from '@/utilities/ui'
+import { cn } from "@/utilities/ui";
 
-import type { BannerBlock as BannerBlockProps } from '@/payload-types'
+import type { BannerBlock as BannerBlockProps } from "@/payload-types";
 
 type Props = {
-  className?: string
-} & BannerBlockProps
+	className?: string;
+} & BannerBlockProps;
 
 export const BannerBlock: React.FC<Props> = ({ className, content, style }) => {
-  return (
-    <div className={cn('mx-auto my-8 w-full', className)}>
-      <div
-        className={cn('border py-3 px-6 flex items-center rounded', {
-          'border-border bg-card': style === 'info',
-          'border-error bg-error/30': style === 'error',
-          'border-success bg-success/30': style === 'success',
-          'border-warning bg-warning/30': style === 'warning',
-        })}
-      >
-        <RichText data={content} enableGutter={false} enableProse={false} />
-      </div>
-    </div>
-  )
-}
+	return (
+		<div className={cn("mx-auto my-8 w-full", className)}>
+			<div
+				className={cn("border py-3 px-6 flex items-center rounded", {
+					"border-border bg-card": style === "info",
+					"border-error bg-error/30": style === "error",
+					"border-success bg-success/30": style === "success",
+					"border-warning bg-warning/30": style === "warning",
+				})}
+			>
+				<RichText data={content} enableGutter={false} enableProse={false} />
+			</div>
+		</div>
+	);
+};
